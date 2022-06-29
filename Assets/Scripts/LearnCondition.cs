872e6b5cfef781d5ef938b420272def8d82c1ba7
KID <50666207+KID421@users.noreply.github.com>
2022-06-29 20:02:30 +0800

    Update 認識 switch 與列舉並定義控制系統資料

--- Assets/Scripts/LearnCondition.cs
+++ Assets/Scripts/LearnCondition.cs
@@ -9,6 +9,21 @@ public class LearnCondition : MonoBehaviour
 {
     public bool openDoor;
     public int combo;
+    public string prop;
+
+    // 1. 定義列舉內容
+    // 2. 定義欄位
+
+    // 列舉語法：
+    // 修飾詞 列舉 列舉名稱 { 列舉內容 }
+    public enum StatePlayer
+    {
+        Idle, Walk, Run, Hurt, Attack, Dead
+    }
+
+    // 定義欄位
+    // 修飾詞 列舉名稱 欄位名稱；
+    public StatePlayer statePlayer;
 
     private void Start()
     {
@@ -24,6 +39,7 @@ public class LearnCondition : MonoBehaviour
 
     private void Update()
     {
+        #region 判斷式 if
         // 如果 openDoor 等於 true 就開門，否則就關門
         // 如果
         // if 語法：
@@ -57,5 +73,62 @@ public class LearnCondition : MonoBehaviour
         {
             print("攻擊力 + 10%");
         }
+        #endregion
+
+        #region 判斷式 switch
+        // switch 語法：
+        // switch (比較的運算式)
+        // {
+        //      case 值:
+        // 　　   程式內容
+        // 　　   break;
+        // }
+
+        switch (prop)
+        {
+            case "紅色藥水":
+                print("補血");
+                break;
+
+            case "藍色藥水":
+                print("補魔");
+                break;
+
+            case "黃色藥水":
+                print("補體力");
+                break;
+
+            // 可省略
+            default:
+                print("沒有此道具~");
+                break;
+        }
+
+        // 1. switch + Tab * 2
+        // 2. 輸入列舉名稱 statePlayer
+        // 3. Enter
+
+        switch (statePlayer)
+        {
+            case StatePlayer.Idle:
+                print("等待");
+                break;
+            case StatePlayer.Walk:
+                print("走路");
+                break;
+            case StatePlayer.Run:
+                print("跑步");
+                break;
+            case StatePlayer.Hurt:
+                print("受傷");
+                break;
+            case StatePlayer.Attack:
+                print("攻擊");
+                break;
+            case StatePlayer.Dead:
+                print("死亡");
+                break;
+        }
+        #endregion
     }
 }
--- Assets/Scripts/SystemControl.cs
+++ Assets/Scripts/SystemControl.cs
@@ -3,9 +3,30 @@ using UnityEngine;
 // 命名空間 namespace 空間名稱 { 該空間的內容 }
 namespace KID
 {
+    /// <summary>
+    /// 玩家控制系統
+    /// 物件旋轉、發射彈珠
+    /// </summary>
     public class SystemControl : MonoBehaviour
     {
+        #region 資料
+        // GameObject 遊戲物件
+        // 存放階層面板內或者專案內的物件
+        [Header("箭頭")]
+        public GameObject arrow;
+        [Header("旋轉速度"), Range(0, 500)]
+        public float speedTurn = 10.5f;
+        [Header("彈珠預製物")]
+        public GameObject marble;
+        [Header("彈珠可以發射的總數"), Range(0, 100)]
+        public int canShootMarbleTotal = 15;
+        #endregion
 
+        #region 事件
+        #endregion
+
+        #region 方法
+        #endregion
     }
 }
 
